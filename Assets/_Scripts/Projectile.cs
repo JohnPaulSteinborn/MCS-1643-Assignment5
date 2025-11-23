ce5f6784506c09651f14c2808d8422474a066ee5
JohnPaulSteinborn <jsteinbor@ltu.edu>
2025-11-23 03:46:29 -0500

    added enemy movement

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -22,8 +22,13 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log($"Hit object {collision.gameObject.transform.name}");
+        //Debug.Log($"Hit object {collision.gameObject.transform.name}");
         //check if this is an enemy
+        Enemy hitEnemy = collision.gameObject.GetComponent<Enemy>();
+        if (hitEnemy != null)
+        {
+            hitEnemy.TakeDamage(Damage);
+        }
         //if it is, notify it of my damage rating
         Destroy(gameObject);
     }
